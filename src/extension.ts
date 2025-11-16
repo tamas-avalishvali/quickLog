6887230a91b591fd6e7d402e916e4193f3096c1d
Behnam <alizadeh3054@gmail.com>
2025-11-16 14:34:52 +0330

    Minor fixes

--- src/extension.ts
+++ src/extension.ts
@@ -18,18 +18,18 @@ export function activate(context: vscode.ExtensionContext) {
     const lineNum = change.range.start.line;
     const lineText = editor.document.lineAt(lineNum).text;
 
-    const match = lineText.match(/(.+)\.glog$/);
-    if (match) {
-      const capturedText = match[1].trim();
-      const range = new vscode.Range(lineNum, 0, lineNum, lineText.length);
+    const match = lineText.match(/(['"`]?)(.+?)\1?\.glog$/);
+    if (!match) return;
 
-      editor.edit(
-        (editBuilder) => {
-          editBuilder.replace(range, `console.log(${capturedText});`);
-        },
-        { undoStopBefore: true, undoStopAfter: true }
-      );
-    }
+    const capturedText = match[2].trim();
+    const range = new vscode.Range(lineNum, 0, lineNum, lineText.length);
+
+    editor.edit(
+      (editBuilder) => {
+        editBuilder.replace(range, `console.log(${capturedText});`);
+      },
+      { undoStopBefore: true, undoStopAfter: true }
+    );
   });
 
   context.subscriptions.push(disposable);
